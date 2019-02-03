de299d2885282126cd4bd452dc720f4ada6691fd
Alexa Duarte <alexa-2095@hotmail.com>
2019-02-02 19:23:26 -0600

    Listo carga de descripción.

--- app/js/models.js
+++ app/js/models.js
@@ -26,58 +26,51 @@ function init() {
         // Parse JSON string into object
         localStorage.setItem("json",response);
 
-        let modelType = localStorage.getItem("model");
-        let kitModel = result.Resistencia;
-
-        if( modelType == "1")
-        {
-            document.getElementById('desc-model-title').innerHTML = kitModel.Foto;
-            document.getElementById('message').innerHTML = kitModel.Descripcion;
-
-            let text="";
-            let size = kitModel.Componentes.length;
-
-            for (let i = 0; i < size; i++) {
-                text += "<a class=\"list-group-item-action\" href=\"ComponentDescription.html\">" +
-                    "<li class=\"list-group-item d-flex justify-content-between align-items-center\" >" + kitModel.Componentes[i] +
-                    "<span class=\"badge badge-primary badge-pill\">"+ kitModel.Cantidad[i]+"</span>" +
-                    "</li>" +
-                    "</a>"
-            }
-            document.getElementById('desc-model-component').innerHTML = text;
-
-        }
-        else if( modelType =="2")
-        {
-            document.getElementById('desc-model-title').innerHTML = kitModel.Titulo;
-            document.getElementById('message').innerHTML = kitModel.Descripcion;
-        }
-        else if( modelType == "3")
-        {
-            document.getElementById('desc-model-title').innerHTML =kitModel.Titulo;
-            document.getElementById('message').innerHTML =kitModel.Descripcion;
-        }
-        else if( modelType == "4")
-        {
-            document.getElementById('desc-model-title').innerHTML =kitModel.Titulo;
-            document.getElementById('message').innerHTML =kitModel.Descripcion;
-        }
-        else if( modelType == "5")
-        {
-            document.getElementById('desc-model-title').innerHTML =kitModel.Titulo;
-            document.getElementById('message').innerHTML =kitModel.Descripcion;
-        }
-        else if( modelType == "6")
-        {
-            document.getElementById('desc-model-title').innerHTML =kitModel.Titulo;
-            document.getElementById('message').innerHTML =kitModel.Descripcion;
-        }
-        else
-        {
-            document.getElementById('desc-model-title').innerHTML =kitModel.Titulo;
-            document.getElementById('message').innerHTML =kitModel.Descripcion;
-        }
+        loadModelInfo();
     });
-
 }
+
+function loadModelInfo() {
+    let modelType = localStorage.getItem("model"); //El modelo que se debe cargar
+    let kitModel; //Modelo escogido en el json.
+    let componentText=""; //Texto de los componentes del modelo (nombre y cantidad)
+    let size;//Número de componentes del modelo.
+
+    switch(modelType) {
+        case "1":
+            kitModel = result.Resistencia;
+            break;
+        case "2":
+            //kitModel = result. ;
+            break;
+        case "3":
+            //kitModel = result. ;
+            break;
+        case "4":
+            //kitModel = result. ;
+            break;
+        case "5":
+            //kitModel = result. ;
+            break;
+        case "6":
+            //kitModel = result. ;
+            break;
+        default:
+        //kitModel = result. ;
+        }
+
+    document.getElementById('desc-model-title').innerHTML = kitModel.Foto; //Titulo del modelo
+    document.getElementById('message').innerHTML = kitModel.Descripcion; //Descripción
+
+    size = kitModel.Componentes.length;
+    for (let i = 0; i < size; i++) { //Itera y almacena cuántos componentes tenga y hace elementos de una lista.
+        componentText += "<a class=\"list-group-item-action\" href=\"ComponentDescription.html\">" +
+            "<li class=\"list-group-item d-flex justify-content-between align-items-center\" >" + kitModel.Componentes[i] +
+            "<span class=\"badge badge-primary badge-pill\">"+ kitModel.Cantidad[i]+"</span>" +
+            "</li>" +
+            "</a>"
+    }
+    document.getElementById('desc-model-component').innerHTML = componentText; //Carga la lista
+}
+
 var result = JSON.parse(localStorage.getItem("json"));
