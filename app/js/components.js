1b341e6191eb29d83ad1b311233a6e6237c8ef48
jeiferp <32427182+jeiferp@users.noreply.github.com>
2019-02-05 23:19:38 -0600

    Resistencias

--- app/js/components.js
+++ app/js/components.js
@@ -4,6 +4,20 @@ function init() {
 
 function loadComponentInfo() {
     let componentType = localStorage.getItem("component"); //El modelo que se debe cargar
-
     document.getElementById('desc-component-title').innerHTML = componentType; //Carga la lista
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("JSONFiles");
+    xobj.open('GET', 'JSONFiles/Components.json', true); // Replace 'my_data' with the path to your file
+    xobj.onreadystatechange = function () {
+        if (xobj.readyState == 4 && xobj.status == "200") {
+            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+            //callback(xobj.responseText);
+            localStorage.setItem("Components",xobj.responseText)
+        }
+    };
+    let components= JSON.parse(localStorage.getItem("Components"))
+    console.log(localStorage)
+    document.getElementById('componentDescription').innerHTML = components.componentType.Descipcion;
+    xobj.send(null);
+
 }
