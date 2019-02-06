416214b7de925606d48b5c9845afcf2b7f30c5ed
Alexa Duarte <alexa-2095@hotmail.com>
2019-02-06 14:10:34 -0600

    Components load

--- app/js/components.js
+++ app/js/components.js
@@ -1,23 +1,31 @@
+function loadJSON(callback) {
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("JSONFiles");
+    xobj.open('GET', 'JSONFiles/Components.json', false); // Replace 'my_data' with the path to your file
+    xobj.onreadystatechange = function () {
+        if (xobj.readyState == 4 && xobj.status == "200") {
+            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+            callback(xobj.responseText);
+        }
+    };
+    xobj.send(null);
+}
+
 function init() {
-   loadComponentInfo();
+    loadJSON(function(response) {
+        // Parse JSON string into object
+        localStorage.setItem("jsonComponents",response);
+
+        loadComponentInfo();
+    });
 }
 
 function loadComponentInfo() {
     let componentType = localStorage.getItem("component"); //El modelo que se debe cargar
     document.getElementById('desc-component-title').innerHTML = componentType; //Carga la lista
-    var xobj = new XMLHttpRequest();
-    xobj.overrideMimeType("JSONFiles");
-    xobj.open('GET', 'JSONFiles/Components.json', true); // Replace 'my_data' with the path to your file
-    xobj.onreadystatechange = function () {
-        if (xobj.readyState == 4 && xobj.status == "200") {
-            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
-            //callback(xobj.responseText);
-            localStorage.setItem("Components",xobj.responseText);
-        }
-    };
-    let components = JSON.parse(localStorage.getItem("Components"));
-    console.log(components);
-    console.log(componentType);
-    alert(components[componentType].Descripcion);
-    document.getElementById('componentDescription').innerHTML = components[componentType].Descripcion;
+    
+    document.getElementById('componentDescription').innerHTML = result[componentType].Descripcion;
 }
+
+var result = JSON.parse(localStorage.getItem("jsonComponents"));
+let components;
