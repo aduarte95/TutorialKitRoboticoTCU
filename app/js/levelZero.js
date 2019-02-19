6fda782aa87eef1d98d05688c4431dcb46f4b0b7
Alexa Duarte <alexa-2095@hotmail.com>
2019-02-19 14:16:53 -0600

    Cargar secciones desde json.

--- app/js/levelZero.js
+++ app/js/levelZero.js
@@ -1,11 +1,14 @@
 let section;
-
+let totalSections = 0;
 function init() {
     loadJSON(function(response) {
         // Parse JSON string into object
         localStorage.setItem("jsonSection",response);
 
         loadSection();
+        for(x in result) {
+            totalSections += result[x]["SubSecciones"].length + 1;
+        }
     });
 }
 
@@ -35,7 +38,7 @@ function loadSection() {
     let section;
     let ref;
 
-    for(index in result) {
+    for (index in result) {
         section = result[index];
         sectionTitle = section["Titulo"];
         src = section["Imagen"];
@@ -51,6 +54,34 @@ function loadSection() {
 
         ++index;
     }
+    document.getElementById('zero-sections').innerHTML = sectionCard; //Carga la lista
+
+}
+
+function loadSectionVariable() {
+    let sectionName = localStorage.getItem("section") //La sección que se debe cargar
+    return result[sectionName];
+}
+function loadSubsection() {
+    let subsectionTitle;
+    let src;
+    let subsectionCard = "";
+    let subsection;
+    let ref = section.html;
+    let sectionType = loadSectionVariable()["SubSecciones"];
+
+    for (index in sectionType) {
+        subsection = sectionType[index];
+        subsectionTitle = sectionType["Titulo"];
+        src = sectionType["Imagen"];
+
+        subsectionCard += doCard(subsectionTitle, src, ref);
+
+        ++index;
+    }
+    document.getElementById('zero-subsections').innerHTML = subsectionCard; //Carga la lista
+
+}
 
     function doCard(sectionTitle, src, ref) {
         return "<div class=\"card text-center\">" +
@@ -64,5 +95,65 @@ function loadSection() {
         "</div>";
     }
 
-    document.getElementById('zero-sections').innerHTML = sectionCard; //Carga la lista
+let count = 0;
+
+function loadSectionDescription() {
+    let sectionName = localStorage.getItem("section") //La sección que se debe cargar
+    let sectionType = result[sectionName];
+
+    console.log(result[sectionName]);
+    console.log("section " +sectionName);
+
+    let title = sectionType["Titulo"];
+    let description = sectionType["Descripcion"];
+    let img = "<img class=\"image-size\" src=\"" + sectionType["Imagen"] + "\" alt=\"" + title +"\">";
+
+    document.getElementById('section-desc-img').innerHTML = img; //Imagen del modelo
+
+    document.getElementById('section-desc-model-title').innerHTML = title; //Titulo del modelo
+    document.getElementById('section-message').innerHTML = description; //Descripción
 }
+
+function loadSectionSteps(step) {
+    let anterior;
+    let bottom;
+    let sectionName = localStorage.getItem("section"); //La sección que se debe cargar
+    let sectionType = result[sectionName];
+
+    count += step;
+
+    if (count != 1) {
+        anterior = "<a class=\"btn btn-primary\" href=\"#\" onclick=\"loadSteps(-1)\" ";
+    } else {
+        count = 1;
+        anterior = "<a class=\"btn btn-primary\" href=\"description.html\" ";
+    }
+
+    let width = (count/sectionType.Pasos.length)*100;
+
+
+    bottom = "<div class=\"col-md-auto\">" +
+        anterior + "role=\"button\"> &laquo; Anterior</a>" +
+        "</div>" +
+        "<div class=\"col\">" +
+        "<div class=\"progress progress-position\">" +
+        "<div class=\"progress-bar progress-bar-striped\" role=\"progressbar\" style=\"width: " + width + "%\" aria-valuenow=\"" + width + "\" aria-valuemin=\"0\" aria-valuemax=\"100\"></div>" +
+        "</div>" +
+        "</div>";
+
+    if(count < sectionType.Pasos.length) {
+        bottom += "<div class=\"col-md-auto text-right\">" +
+            "<a class=\"btn btn-primary\" href=\"#\" role=\"button\" onclick=\"loadSteps(1)\">Siguiente &raquo;</a>" +
+            "</div>";
+    } else if (count < totalSections){
+        bottom += "<div class=\"col-md-auto text-right\">" +
+            "<a class=\"btn btn-primary\" href=\"#\" role=\"button\" onclick=\"setSection(result[sectionName+1]);loadSectionDescription();\">Siguiente &raquo;</a>" +
+            "</div>";
+    }
+
+    document.getElementById('section-step-title').innerHTML = sectionType.Pasos[count - 1].Title;
+    document.getElementById('section-step-desc').innerHTML = sectionType.Pasos[count - 1].Descripcion;
+    document.getElementById('section-step-image').innerHTML = "<img class=\"image-size\" src=\"" + sectionType.Pasos[count - 1].Imagen + "\" alt=\""+ kitModel.Pasos[count - 1].Encabezado + "\">";
+    document.getElementById('section-step-bottom').innerHTML = bottom;
+}
+
